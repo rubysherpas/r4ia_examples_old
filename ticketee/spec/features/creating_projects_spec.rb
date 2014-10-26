9279a78eb01fdae9b2a66ed85d78165363e232f1
Ryan Bigg <radar@lifx.co>
2014-10-26 20:57:23 +1100

    Section 3.4.3: 'Create a new project' feature complete.

---
+++ ticketee/spec/features/creating_projects_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'Creating Projects' do
+  scenario "can create a project" do
+    visit '/'
+    click_link 'New Project'
+    fill_in 'Name', with: 'Sublime Text 3'
+    fill_in 'Description', with: 'A text-editor for all operating systems'
+    click_button 'Create Project'
+    expect(page).to have_content('Project has been created.')
+  end
+end
