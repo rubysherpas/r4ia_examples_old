ccd88fd3ea8a380932a78598521e4dcdfa62ec23
Ryan Bigg <radar@lifx.co>
2014-11-04 21:20:35 +1100

    Section 5.3.2: We can now edit tickets

--- ticketee/app/controllers/tickets_controller.rb
+++ ticketee/app/controllers/tickets_controller.rb
@@ -18,6 +18,18 @@ class TicketsController < ApplicationController
     end
   end
 
+  def update
+    if @ticket.update(ticket_params)
+      flash[:notice] = "Ticket has been updated."
+
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been updated."
+
+      render action: "edit"
+    end
+  end
+
   private
     def ticket_params
       params.require(:ticket).permit(:title, :description)
