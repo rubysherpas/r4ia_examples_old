6a9b6b18b7658a7d2d45081d0b6fd2630866ffeb
Ryan Bigg <radar@lifx.co>
2014-11-04 12:39:26 +1100

    Section 4.4.2: Redirect the users back to the projects page if they try going to a project that does not exist

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -1,4 +1,8 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show,
+                                     :edit,
+                                     :update,
+                                     :destroy]
   def index
     @projects = Project.all
   end
@@ -19,17 +23,7 @@ class ProjectsController < ApplicationController
     end
   end
 
-  def show
-    @project = Project.find(params[:id])
-  end
-
-  def edit
-    @project = Project.find(params[:id])
-  end
-
   def update
-    @project = Project.find(params[:id])
-
     if @project.update(project_params)
       flash[:notice] = "Project has been updated."
       redirect_to @project
@@ -40,7 +34,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
 
     flash[:notice] = "Project has been destroyed."
@@ -49,6 +42,15 @@ class ProjectsController < ApplicationController
   end
 
   private
+
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking" +
+                    " for could not be found."
+
+    redirect_to projects_path
+  end
 
   def project_params
     params.require(:project).permit(:name, :description)
