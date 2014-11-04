1793444ebe97fb143ac4346a5b1aa6aff0e9c01f
Ryan Bigg <radar@lifx.co>
2014-11-04 21:00:08 +1100

    Section 5.1.6 + Section 5.2.1: Implement creating tickets for a project

---
+++ ticketee/spec/features/viewing_tickets_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+feature "Viewing tickets" do
+  before do
+    sublime = FactoryGirl.create(:project, name: "Sublime Text 3")
+
+    FactoryGirl.create(:ticket,
+      project: sublime,
+      title: "Make it shiny!",
+      description: "Gradients! Starbursts! Oh my!")
+
+    ie = FactoryGirl.create(:project, name: "Internet Explorer")
+    FactoryGirl.create(:ticket,
+      project: ie,
+      title: "Standards compliance",
+      description: "Isn't a joke.")
+
+    visit '/'
+  end
+
+  scenario "Viewing tickets for a given project" do
+    click_link "Sublime Text 3"
+
+    expect(page).to have_content("Make it shiny!")
+    expect(page).to_not have_content("Standards compliance")
+
+    click_link "Make it shiny!"
+    within("#ticket h2") do
+      expect(page).to have_content("Make it shiny!")
+    end
+
+    expect(page).to have_content("Gradients! Starbursts! Oh my!")
+  end
+end
