42fcc5e28e1b25476cee23a8f4d482dbc884479c
Ryan Bigg <radar@lifx.co>
2014-11-04 12:16:02 +1100

    Section 4.2.2: Projects can now be updated

---
+++ ticketee/spec/features/editing_projects_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+feature "Editing Projects" do
+  before do
+    FactoryGirl.create(:project, name: "Sublime Text 3")
+
+    visit "/"
+    click_link "Sublime Text 3"
+    click_link "Edit Project"
+  end
+
+  scenario "Updating a project" do
+    fill_in "Name", with: "Sublime Text 3 beta"
+    click_button "Update Project"
+
+    expect(page).to have_content("Project has been updated.")
+  end
+
+  scenario "Updating a project with invalid attributes is bad" do
+    fill_in "Name", with: ""
+    click_button "Update Project"
+
+    expect(page).to have_content("Project has not been updated.")
+  end
+end
