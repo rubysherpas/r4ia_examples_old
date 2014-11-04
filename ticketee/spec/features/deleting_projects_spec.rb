8bdd203c1307c01b07c5fe38ae8940767f66a450
Ryan Bigg <radar@lifx.co>
2014-11-04 12:21:22 +1100

    Section 4.3: Projects can now be deleted

---
+++ ticketee/spec/features/deleting_projects_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature "Deleting projects" do
+  scenario "Deleting a project" do
+    FactoryGirl.create(:project, name: "Sublime Text 3")
+
+    visit "/"
+    click_link "Sublime Text 3"
+    click_link "Delete Project"
+
+    expect(page).to have_content("Project has been destroyed.")
+
+    visit "/"
+
+    expect(page).to have_no_content("Sublime Text 3")
+  end
+end
