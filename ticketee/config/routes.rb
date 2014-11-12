01aaacd0c2b55c324ed93a6aac678f2f20e84486
Ryan Bigg <radar@lifx.co>
2014-11-12 13:59:56 +1100

    Section 6.3: Implemented sign up

--- ticketee/config/routes.rb
+++ ticketee/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'projects#index'
+  get "/users/sign_up", to: "users#new", as: "sign_up"
+  post "/users/sign_up", to: "users#create"
 
   resources :projects do
     resources :tickets
