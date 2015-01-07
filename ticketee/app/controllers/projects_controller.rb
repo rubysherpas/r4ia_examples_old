e864cecb3136e5cc7a0fd6a8fcf8565cc1a87493
Ryan Bigg <radar@lifx.co>
2015-01-07 18:10:40 +1100

    Only admins can create or delete projects

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -1,26 +1,8 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show,
-                                     :edit,
-                                     :update,
-                                     :destroy]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
-  end
-
-  def new
-    @project = Project.new
-  end
-
-  def create
-    @project = Project.new(project_params)
-
-    if @project.save
-      flash[:notice] = "Project has been created."
-      redirect_to @project
-    else
-      flash[:alert] = "Project has not been created."
-      render "new"
-    end
   end
 
   def update
