81bc718de8c85eb692a9cf375760752048019f7d
Ryan Bigg <radar@lifx.co>
2015-01-08 08:46:15 +1100

    Section 8.1.7: Don't show projects that a user doesn't have permission to see

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Project.all
+    @projects = policy_scope(Project)
   end
 
   def update
