f67d3aa905ab5af480ea7e839f0403a86410f413
Ryan Bigg <radar@lifx.co>
2015-01-07 18:20:41 +1100

    Section 7.3.2: Only admins can see the links to create and delete projects

---
+++ ticketee/spec/features/hidden_links_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+RSpec.feature "hidden links" do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:admin) { FactoryGirl.create(:user, :admin) }
+  let(:project) { FactoryGirl.create(:project) }
+
+  context "anonymous users" do
+    scenario "cannot see the New Project link" do
+      visit "/"
+      expect(page).not_to have_link "New Project"
+    end
+
+    scenario "cannot see the Delete Project link" do
+      visit project_path(project)
+      expect(page).not_to have_link "Delete Project"
+    end
+  end
+
+  context "regular users" do
+    before { login_as(user) }
+    scenario "cannot see the New Project link" do
+      visit "/"
+      expect(page).not_to have_link "New Project"
+    end
+
+    scenario "cannot see the Delete Project link" do
+      visit project_path(project)
+      expect(page).not_to have_link "Delete Project"
+    end
+  end
+
+  context "admin users" do
+    before { login_as(admin) }
+    scenario "can see the New Project link" do
+      visit "/"
+      expect(page).to have_link "New Project"
+    end
+
+    scenario "can see the Delete Project link" do
+      visit project_path(project)
+      expect(page).to have_link "Delete Project"
+    end
+  end
+end
